1b62ea4a38e6ad16cc1e0c25299502ffbcdb778a
Nsikan Ikpoh <nsikanikpoh@gmail.com>
2020-04-08 22:18:09 +0100

    adding changes from assessment branch

--- src/estimator.js
+++ src/estimator.js
@@ -31,36 +31,38 @@ const covid19ImpactEstimator = (data) => {
   const impactSevereCasesByRequestedTime = percentageEstimator(15, impactEstimate);
   const severeImpactSevereCasesByRequestedTime = percentageEstimator(15, severeImpactEstimate);
   const bedAvailabitlity = percentageEstimator(35, input.totalHospitalBeds);
-  const impactHospitalBedsByRequestedTime = parseInt(bedAvailabitlity) - impactSevereCasesByRequestedTime;
-  const severeImpactHospitalBedsByRequestedTime = parseInt(bedAvailabitlity) - severeImpactSevereCasesByRequestedTime;
+  const impactHospitalBeds = parseInt(bedAvailabitlity) - impactSevereCasesByRequestedTime;
+  const severeImpactHospitalBeds = (parseInt(bedAvailabitlity)
+                                          - severeImpactSevereCasesByRequestedTime);
 
   //  Challenge 3
   const impactCasesForICUByRequestedTime = percentageEstimator(5, impactEstimate);
   const severeImpactCasesForICUByRequestedTime = percentageEstimator(5, severeImpactEstimate);
-  const impactCasesForVentilatorsByRequestedTime = percentageEstimator(2, impactEstimate);
-  const severeImpactCasesForVentilatorsByRequestedTime = percentageEstimator(2, severeImpactEstimate);
-  const impactDollarsInFlight = impactEstimate * input.region.avgDailyIncomeInUSD * input.region.avgDailyIncomePopulation * days;
-  const severeImpactDollarsInFlight = (severeImpactEstimate * input.region.avgDailyIncomeInUSD *
-                                        input.region.avgDailyIncomePopulation * days);
+  const impactCasesForVentilators = percentageEstimator(2, impactEstimate);
+  const severeImpactCasesForVentilators = percentageEstimator(2, severeImpactEstimate);
+  const impactDollarsInFlight = (impactEstimate * input.region.avgDailyIncomeInUSD
+                                    * input.region.avgDailyIncomePopulation * days);
+  const severeImpactDollarsInFlight = (severeImpactEstimate * input.region.avgDailyIncomeInUSD
+                                          * input.region.avgDailyIncomePopulation * days);
 
-  return({
+  return ({
     data: input,
     impact: {
       currentlyInfected: impactCurentlyInfected,
       infectionsByRequestedTime: impactEstimate,
       severeCasesByRequestedTime: impactSevereCasesByRequestedTime,
-      hospitalBedsByRequestedTime: impactHospitalBedsByRequestedTime,
+      hospitalBedsByRequestedTime: impactHospitalBeds,
       casesForICUByRequestedTime: impactCasesForICUByRequestedTime,
-      casesForVentilatorsByRequestedTime: impactCasesForVentilatorsByRequestedTime,
+      casesForVentilatorsByRequestedTime: impactCasesForVentilators,
       dollarsInFlight: impactDollarsInFlight
     },
     severeImpact: {
       currentlyInfected: severeImpactCurentlyInfected,
       infectionsByRequestedTime: severeImpactEstimate,
       severeCasesByRequestedTime: severeImpactSevereCasesByRequestedTime,
-      hospitalBedsByRequestedTime: severeImpactHospitalBedsByRequestedTime,
+      hospitalBedsByRequestedTime: severeImpactHospitalBeds,
       casesForICUByRequestedTime: severeImpactCasesForICUByRequestedTime,
-      casesForVentilatorsByRequestedTime: severeImpactCasesForVentilatorsByRequestedTime,
+      casesForVentilatorsByRequestedTime: severeImpactCasesForVentilators,
       dollarsInFlight: severeImpactDollarsInFlight
     }
   });
