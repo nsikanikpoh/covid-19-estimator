c80c133acb0eb3b9011e96c775eef29cec7bde3b
Nsikan Ikpoh <nsikanikpoh@gmail.com>
2020-04-09 21:24:54 +0100

    adding changes to assessment

--- src/estimator.js
+++ src/estimator.js
@@ -43,7 +43,7 @@ const covid19ImpactEstimator = (data) => {
   //  Challenge 3
   const impactCasesForICUByRequestedTime = Math.floor(percentageEstimator(5, impactEstimate));
   const severeImpactCasesForICUByRequestedTime = Math.floor(percentageEstimator(5,
-      severeImpactEstimate));
+    severeImpactEstimate));
   const impactCasesForVentilators = Math.floor(percentageEstimator(2, impactEstimate));
   const severeImpactCasesForVentilators = Math.floor(percentageEstimator(2, severeImpactEstimate));
   const impactDollarsInFlight = Math.floor((impactEstimate * input.region.avgDailyIncomeInUSD
